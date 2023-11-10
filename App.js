6f828afbf74e90f9c9afe9217f286aec772ea44e
kacperkapusciak <kacper.kapusciak@swmansion.com>
2023-11-10 12:21:17 +0100

    Upload project files

--- App.js
+++ App.js
@@ -1,46 +1,69 @@
+import { View, Text, StyleSheet } from "react-native";
 import Animated, {
-  useSharedValue,
-  withTiming,
+  useAnimatedSensor,
+  SensorType,
   useAnimatedStyle,
-  Easing,
+  withSpring,
 } from "react-native-reanimated";
-import { View, Button } from "react-native";
 
-export default function AnimatedStyleUpdateExample(props) {
-  const randomWidth = useSharedValue(10);
+export default function App() {
+  const rotation = useAnimatedSensor(SensorType.ROTATION, {
+    interval: 20,
+  });
 
-  const config = {
-    duration: 500,
-    easing: Easing.bezier(0.5, 0.01, 0, 1),
-  };
-
-  const style = useAnimatedStyle(() => {
+  const foregroundStyle = useAnimatedStyle(() => {
+    const { pitch, roll } = rotation.sensor.value;
     return {
-      width: withTiming(randomWidth.value, config),
+      transform: [
+        { translateX: withSpring(-roll * 50, { damping: 200 }) },
+        { translateY: withSpring(-pitch * 50, { damping: 200 }) },
+      ],
+    };
+  });
+
+  const backgroundStyle = useAnimatedStyle(() => {
+    const { pitch, roll } = rotation.sensor.value;
+    return {
+      transform: [
+        { translateX: withSpring(-roll * 25, { damping: 200 }) },
+        { translateY: withSpring(-pitch * 25, { damping: 200 }) },
+      ],
     };
   });
 
   return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-        flexDirection: "column",
-      }}
-    >
-      <Animated.View
-        style={[
-          { width: 100, height: 80, backgroundColor: "black", margin: 30 },
-          style,
-        ]}
+    <View style={styles.container}>
+      <Animated.Image
+        style={backgroundStyle}
+        source={require("./assets/background.jpg")}
       />
-      <Button
-        title="toggle"
-        onPress={() => {
-          randomWidth.value = Math.random() * 350;
-        }}
+      <Animated.Image
+        style={[styles.foreground, foregroundStyle]}
+        source={require("./assets/foreground.png")}
       />
+      <Text style={styles.text}>Visit Ireland</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  foreground: {
+    position: "absolute",
+    width: "120%",
+    height: "100%",
+    bottom: -50,
+  },
+  text: {
+    fontWeight: "bold",
+    fontSize: 40,
+    textAlign: "center",
+    color: "white",
+    position: "absolute",
+    top: 150,
+  },
+});
